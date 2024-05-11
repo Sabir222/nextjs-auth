ddb0b0bc48084e7fa7ece2bf7fa58a39cafd1605
sabir222 <mrsabir4@gmail.com>
2024-05-11 22:34:48 +0100

    adding navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-full">
       <div>
-        <Button>Hello world!</Button>
+        <h1 className="drop-shadow-lg space-y-6 text-5xl text-center">
+          Not Authenticated
+        </h1>
       </div>
     </main>
   );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./Toggle";
 
 const NavBar = () => {
   return (
-    <nav className="fixed right-0 left-0 h-14  border-b-black dark:border-b-white/10 border-b-[0.5px] p-2">
+    <nav className="fixed right-0 left-0 h-14  border-b-black/20 dark:border-b-white/10 border-b-[0.5px] p-2">
       <main className="max-w-7xl mx-auto ">
         <ModeToggle />
       </main>
