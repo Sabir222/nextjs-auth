e0465592ca2cd7abc31523ff100df6a790830bbe
sabir222 <mrsabir4@gmail.com>
2024-06-01 22:04:36 +0100

    update: Credential

--- auth.config.ts
+++ auth.config.ts
@@ -3,8 +3,18 @@ import type { NextAuthConfig } from "next-auth";
 import { SignInSchema } from "./schemas";
 import { getUserByEmail } from "./lib/user";
 import bcrypt from "bcryptjs";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFiels = SignInSchema.safeParse(credentials);
--- components/cards/SignUpCard.tsx
+++ components/cards/SignUpCard.tsx
@@ -16,22 +16,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { signUp } from "@/actions/signUp";
 import MessageAuth from "./Error-auth";
-
-const formSchema = z
-  .object({
-    username: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-    password: z
-      .string()
-      .min(6, { message: "Password must be atleast 6 characters" }),
-    confirm: z.string(),
-    email: z.string().email({ message: "Use a valid email" }),
-  })
-  .refine((data) => data.password === data.confirm, {
-    message: "Passwords don't match",
-    path: ["confirm"],
-  });
+import { SignUpSchema } from "@/schemas";
 type Message = {
   error: string | undefined;
   success: string | undefined;
@@ -43,14 +28,14 @@ const SignUpCard = () => {
     success: undefined,
   });
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
     defaultValues: {
-      username: "",
+      name: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignUpSchema>) {
     startTransition(() => {
       signUp(values).then((res) => {
         setMessage({ error: res.error, success: res.sucess });
@@ -71,7 +56,7 @@ const SignUpCard = () => {
           <FormField
             disabled={isPending}
             control={form.control}
-            name="username"
+            name="name"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Username</FormLabel>
